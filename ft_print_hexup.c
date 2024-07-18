62b505df2809a3449769ba1175f8d1a079efb219
Mikolaj Jakowicki <mjakowic@c3r15s3.42warsaw.pl>
2024-07-18 18:45:53 +0200

    first commit

---
+++ ft_print_hexup.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_hexup.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjakowic <mjakowic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 14:06:55 by mjakowic          #+#    #+#             */
+/*   Updated: 2024/03/29 13:22:56 by mjakowic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stdio.h>
+
+static int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_print_hexup(unsigned int n)
+{
+	int	length;
+
+	length = 0;
+	if (n >= 16)
+	{
+		length += ft_print_hexup(n / 16);
+		length += ft_print_hexup(n % 16);
+	}
+	else
+	{
+		if (n <= 9)
+			length += ft_putchar(n + '0');
+		else
+			length += ft_putchar(n - 10 + 'A');
+	}
+	return (length);
+}
+
+int	main(void)
+{
+	unsigned int	number;
+
+	number = 3054464646;
+	ft_print_hexup(number);
+	ft_putchar('\n');
+	printf("%X", number);
+	return (0);
+}
+
